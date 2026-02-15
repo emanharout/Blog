52b2608c1d8529c8fc8f68c28f4f5170f0b2529a
Eman Harout <emanharout@gmail.com>
2026-02-14 22:57:06 -0800

    Upgrade Tailwind to v4 with Astro Vite plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
+import typography from '@tailwindcss/typography'
 /** @type {import('tailwindcss').Config} */
 
 export default {
@@ -193,6 +194,6 @@ export default {
 		},
 	},
 	plugins: [
-		require('@tailwindcss/typography'),
+		typography,
 	],
 }
